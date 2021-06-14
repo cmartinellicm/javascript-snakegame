d639bb33894e099248c875b8952f2fd6ae7d19a9
cmartinellicm <martinelli.cassia@gmail.com>
2021-06-14 16:59:15 -0300

    Added game pause

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Recreating snake game with JavaScript
-Project developed during SPTech Front-end Development Bootcamp on [Digital Innovation One platform](https://digitalinnovation.one/).
+Project developed during 'SPTech Front-end Development Bootcamp' on [Digital Innovation One platform](https://digitalinnovation.one/).
 
-## Changes made from original project:
+## Changes made from original project
 
-- Changed food from square to circle;
-- 
+- Food from square to circle;
+- Added font from Google Fonts;
+- Changed background and font color;
+- Added rounded border to canvas;
+- Added alert to pause game;
--- script.js
+++ script.js
@@ -12,18 +12,19 @@ let food = {
   y: Math.floor(Math.random() * 15 + 1) * box,
 };
 
-function criarBG() {
+function createBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha() {
+function creteSnake() {
   for (i = 0; i < snake.length; i++) {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
+// Create food at a random spot
 function drawFood() {
   context.fillStyle = "red";
   // context.fillRect(food.x, food.y, box, box);
@@ -35,18 +36,27 @@ function drawFood() {
 document.addEventListener("keydown", update);
 
 function update(event) {
+  // Update direction based on pressed key
   if (event.keyCode == 37 && direction != "right") direction = "left";
   if (event.keyCode == 38 && direction != "down") direction = "up";
   if (event.keyCode == 39 && direction != "left") direction = "right";
   if (event.keyCode == 40 && direction != "up") direction = "down";
+  // Pause game
+  if (event.keyCode == 32) alert("Game paused. Press ok to continue.");
 }
 
-function iniciarJogo() {
+function startGame() {
+  createBG();
+  creteSnake();
+  drawFood();
+
+  //Check if snake is out of canvas
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+  // End game if snake's head touches its body
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       clearInterval(jogo);
@@ -54,18 +64,16 @@ function iniciarJogo() {
     }
   }
 
-  criarBG();
-  criarCobrinha();
-  drawFood();
-
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
+  //Make snake's head move according to direction
   if (direction == "right") snakeX += box;
   if (direction == "left") snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
+  // If snake doesn't eat food it stays the same size, otherwise snake will grow and food will appear elsewhere
   if (snakeX != food.x || snakeY != food.y) {
     snake.pop();
   } else {
@@ -81,4 +89,4 @@ function iniciarJogo() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(startGame, 100);
